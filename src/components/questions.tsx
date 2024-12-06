b1bfec38eec0549ec3c6d7e0de54365a329314ea
Michał Worwąg <michalwo@gmail.com>
2024-12-06 19:13:43 +0100

    Remove outdated JavaScript question pages and update sorting logic; add new questions on JavaScript and React features

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { getQuestions } from '../libs/get-questions';
+import Published from './published';
 
 export async function Questions({ category }: { category: string }) {
   const questions = await getQuestions(category);
@@ -11,17 +12,16 @@ export async function Questions({ category }: { category: string }) {
             <li key={slug} className='pl-1'>
               <h2 className='text-gray-800 text-lg font-medium mt-8 mb-3'>
                 <Link
-                  href={`/questions/javascript/${slug}`}
+                  href={`/questions/${category}/${slug}`}
                   className='text-blue-500 hover:text-blue-700'
                 >
                   {title}
                 </Link>
               </h2>
-              <p className='text-gray-800 leading-snug'>{description}</p>
-              <p className='text-gray-800 leading-snug mt-2'>
-                Published:
-                {new Date(publishDate).toLocaleDateString()}{' '}
-              </p>
+              {description ?? (
+                <p className='text-gray-800 leading-snug'>{description}</p>
+              )}
+              <Published date={publishDate} />
             </li>
           ))}
         </ol>
